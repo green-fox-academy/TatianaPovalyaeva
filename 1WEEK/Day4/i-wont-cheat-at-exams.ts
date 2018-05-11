a27d28b36cc7f6ee50467dd667b694e44ca40346
Tanya <povalyaeva@hotmail.co.uk>
2018-05-11 10:25:49 +0200

    100 line statement

---
+++ 1WEEK/Day4/i-wont-cheat-at-exams.ts
@@ -0,0 +1,9 @@
+'use strict';
+export {};
+for (let i = 0; i < 100; i++) {
+   console.log("I won't cheat on the exam!");
+}
+
+/*let i = 1; i <=100
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"*/
