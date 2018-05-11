fcecd580cc38f66d62b3990c09d885d34c8a61e4
Tanya <povalyaeva@hotmail.co.uk>
2018-05-11 10:42:04 +0200

    for loop for printing out odd numbers

---
+++ 1WEEK/Day4/print-even.ts
@@ -0,0 +1,15 @@
+'use strict';
+export {};
+for (let i = 0; i <= 500; i++) {
+    if (i % 2 === 0) {
+        console.log (i);
+    }
+}
+
+/*
+for (let i = 0; i <= 500; i+=2) {
+    console.log (i);
+    }
+MORE EFFICIENT*/
+
+// Create a program that prints all the even numbers between 0 and 500
