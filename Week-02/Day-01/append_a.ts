4c67ee2381ad4d9d9993cdd92f3aa53109345de7
Tanya <povalyaeva@hotmail.co.uk>
2018-05-14 17:22:31 +0200

    Append an array of words

---
+++ Week-02/Day-01/append_a.ts
@@ -0,0 +1,12 @@
+'use strict';
+export {};
+
+let animals: string[] = ["kuty", "macsk", "cic"].map(function(input) {
+    return input + 'a' ;
+  });
+  console.log(animals);
+
+
+// -  Create an array variable named `animals`
+//    with the following content: `["kuty", "macsk", "cic"]`
+// -  Add all elements an `"a"` at the end
