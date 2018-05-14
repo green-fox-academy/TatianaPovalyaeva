f3f32926bce93b114faea14ca97bc26f8e277787
Tanya <povalyaeva@hotmail.co.uk>
2018-05-14 15:23:04 +0200

    Finding an element in an array of numbers

---
+++ Week-02/Day-01/third.ts
@@ -0,0 +1,10 @@
+'use strict';
+export {};
+
+let magicNUmbers: number[] = [1, 3, 178, 5, 7, 11, 23, 78];
+console.log(magicNUmbers[2]);
+
+
+// -  Create a variable named `magicNumbers`
+//    with the following content: `[1, 3, 5, 7]`
+// -  Print the third element of `magicNumbers`
